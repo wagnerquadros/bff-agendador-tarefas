14751f493c5116046d447996facf61f14ddff750
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-08-12 13:30:09 -0300

    Commit inicial

---
+++ src/main/java/com/wagnerquadros/bffagendador/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerquadros.bffagendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
