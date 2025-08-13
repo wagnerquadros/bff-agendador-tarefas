12ecad350967a2a229c470b8b5a9a7b86de4154e
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-08-13 17:10:58 -0300

    Titulo Swegger

--- src/main/java/com/wagnerquadros/bffagendador/BffAgendadorApplication.java
+++ src/main/java/com/wagnerquadros/bffagendador/BffAgendadorApplication.java
@@ -1,8 +1,17 @@
 package com.wagnerquadros.bffagendador;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Agendador de Tarefas"
+		)
+)
+
 
 @SpringBootApplication
 @EnableFeignClients
