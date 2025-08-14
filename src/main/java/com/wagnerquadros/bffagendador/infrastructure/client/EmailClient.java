8441d1694e863982b8a1cd0c714595a5749ec19a
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-08-14 17:57:29 -0300

    Criada EmailClient e EmailService

--- src/main/java/com/wagnerquadros/bffagendador/infrastructure/client/EmailClient.java
+++ src/main/java/com/wagnerquadros/bffagendador/infrastructure/client/EmailClient.java
@@ -1,2 +1,12 @@
-package com.wagnerquadros.bffagendador.infrastructure.client;public interface EmailClient {
+package com.wagnerquadros.bffagendador.infrastructure.client;
+
+import com.wagnerquadros.bffagendador.business.dto.out.TarefasResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    Void enviarEmail(@RequestBody TarefasResponseDTO dto);
 }
