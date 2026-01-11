5d3cb3ab60813ccf46908fe8fa35d26dd700db46
wagnerquadros <woquadros@gmail.com>
2026-01-11 10:14:30 -0300

    Adicionado busca de CEP

--- src/main/java/com/wagnerquadros/bffagendador/infrastructure/client/config/FeignError.java
+++ src/main/java/com/wagnerquadros/bffagendador/infrastructure/client/config/FeignError.java
@@ -2,24 +2,45 @@ package com.wagnerquadros.bffagendador.infrastructure.client.config;
 
 import com.wagnerquadros.bffagendador.infrastructure.exceptions.BusinessException;
 import com.wagnerquadros.bffagendador.infrastructure.exceptions.ConflictException;
+import com.wagnerquadros.bffagendador.infrastructure.exceptions.IllegalArgumentException;
 import com.wagnerquadros.bffagendador.infrastructure.exceptions.ResourceNotFoundException;
 import com.wagnerquadros.bffagendador.infrastructure.exceptions.UnauthorizedException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
 public class FeignError implements ErrorDecoder {
 
     @Override
     public Exception decode(String s, Response response) {
+
+        String mensagemErro = mensagemErro(response);
+
         switch (response.status()) {
             case 409:
-                return new ConflictException("Erro atributo já existente");
+                return new ConflictException("Erro: " + mensagemErro);
             case 403:
-                return new ResourceNotFoundException("Erro atributo não encontrado");
+                return new ResourceNotFoundException("Erro: " + mensagemErro);
             case 401:
-                return new UnauthorizedException("Erro usuário não autorizado");
+                return new UnauthorizedException("Erro: " + mensagemErro);
+            case 400:
+                return new IllegalArgumentException("Erro: " + mensagemErro);
             default:
-                return new BusinessException("Erro de servidor");
+                return new BusinessException("Erro: " + mensagemErro);
+        }
+    }
+
+    private String mensagemErro(Response response){
+        try {
+            if(Objects.isNull(response.body())){
+                return "";
+            }
+            return new String(response.body().asInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
